8d5c02ca89730d415855852af7add7a494df5d77
Abhinav <abhinavponnu964531@gmail.com>
2025-10-03 20:20:46 +0530

    Added a notification page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,21 +4,32 @@ import { Home } from "../pages";
 import { SearchPage } from "../pages";
 import JobResults from "../pages/JobPage";
 import DateFilterSheet from "../features/jobs/components/DateFilterSheet";
+import NotificationPage from "../pages/NotificationPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/search" element={<SearchPage />}/>
+      {/* <Route path="/search" element={<SearchPage />}/> */}
       <Route path="/jobs" element={<JobResults />} />
-      <Route path="/filter/date" element={<DateFilterSheet isOpen={true} onClose={function (): void {
-        throw new Error("Function not implemented.");
-      } } onApply={function (selected: string): void {
-        throw new Error("Function not implemented.");
-      } } />} />
+      <Route
+        path="/filter/date"
+        element={
+          <DateFilterSheet
+            isOpen={true}
+            onClose={function (): void {
+              throw new Error("Function not implemented.");
+            }}
+            onApply={function (selected: string): void {
+              throw new Error("Function not implemented.");
+            }}
+          />
+        }
+      />
+      <Route path="/notification" element={<NotificationPage />} />
       <Route element={<ProtectedRoute />}>
         {/* All protected routes should be here */}
-        
+
         {/* <Route path="/profile" element={<Profile />} /> */}
       </Route>
     </Routes>
